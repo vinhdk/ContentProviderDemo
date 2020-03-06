c7e7555cfeb64f03a0cc62ea3887cd6a005b9640
vinhdk <vinhdk@github.com>
2020-03-06 16:38:04 +0700

    commit

--- app/src/main/java/se1302/demo/contentproviderdemo/MainActivity.java
+++ app/src/main/java/se1302/demo/contentproviderdemo/MainActivity.java
@@ -53,6 +53,12 @@ public class MainActivity extends AppCompatActivity {
         ArrayList<StudentModel> students = new ArrayList<StudentModel>();
         if (cursor != null) {
             if (cursor.moveToFirst()) {
+                students.add(new StudentModel(
+                        cursor.getString(cursor.getColumnIndex("id")),
+                        cursor.getString(cursor.getColumnIndex("name")),
+                        cursor.getInt(cursor.getColumnIndex("core")),
+                        cursor.getInt(cursor.getColumnIndex("isGraduate"))
+                ));
                 while (cursor.moveToNext()) {
                     StudentModel student = new StudentModel(
                             cursor.getString(cursor.getColumnIndex("id")),
